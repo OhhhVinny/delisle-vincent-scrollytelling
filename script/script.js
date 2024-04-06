548d409414f1476bbc71af81273b8e7b193707e7
Vincent Delisle <112128292+OhhhVinny@users.noreply.github.com>
2024-04-05 23:43:00 -0400

    Importation de P2.2

---
+++ script/script.js
@@ -0,0 +1,17 @@
+gsap.to(".scroll-down", {
+  y: 10,
+  duration: 1,
+  repeat: -1,
+  yoyo: true,
+  ease: "power1.inOut",
+});
+
+window.addEventListener("scroll", function () {
+  document.body.classList.add("is-scrolling");
+
+  clearTimeout(timer);
+
+  var timer = setTimeout(function () {
+    document.body.classList.remove("is-scrolling");
+  }, 100);
+});
